b594427eaa136878b360513c35ed2b15366d4b81
mcMEJIA <mejiasebastian1499@gmail.com>
2020-11-23 20:17:40 -0500

    critical changes

--- api/db/create.sql
+++ api/db/create.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE evergreen;
 USE evergreen;
-<<<<<<< HEAD
 CREATE TABLE mensaje (
 id_men int(255) primary key auto_increment,
 asunto varchar(200) not null,
@@ -12,15 +11,3 @@ canal varchar(200) not null,
 estado varchar(200)
 );
 INSERT INTO mensaje(asunto, cuerpo, remitente,destinatario, canal, estado) VALUES ('Mensaje URGENTE', 'Es necesaria su presencia en la Oficina principal', 'jefe@jefe.com', 'bademployee@bademployee.com','Correo','Enviado');
-=======
-CREATE TABLE Canal (
-	id_canal INT(11) PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(20) NOT NULL,
-    tipo VARCHAR(20) NOT NULL,
-    tipo_destinatario VARCHAR(20) NOT NULL,
-    fecha DATETIME
-)
-INSERT INTO canal(nombre, tipo, tipo_destinatario,fecha) VALUES ('Mensaje individual', 'SMS', 'Simple', '2020/07/15');
-INSERT INTO canal(nombre, tipo, tipo_destinatario,fecha) VALUES ('Correo', 'Email', 'Grupo', '2020/06/22');
-INSERT INTO canal(nombre, tipo, tipo_destinatario,fecha) VALUES ('Llamada', 'Central Telefonica', 'Simple', '2020/08/21');
->>>>>>> 4a410c7b4644a90b5a743415655b0d8f0820a488
--- api/server.py
+++ api/server.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 from flask import Flask , request
 from flask import jsonify, request
 from flask_cors import CORS
 from controllers.mensaje import Mensaje
-=======
-from flask import Flask, request
-from flask_cors import CORS
-from controllers.Canal import Canal
->>>>>>> 4a410c7b4644a90b5a743415655b0d8f0820a488
 from db.db import cnx
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 @app.route('/mensaje', methods=['GET'])
 def getall():
     return (Mensaje.list())
@@ -32,22 +25,6 @@ def put():
     body = request.json
     return (Mensaje.put(body))
 
-=======
-
-@app.route('/canal', methods=['GET'])
-def getAll():
-    return (Canal.get())
-
-@app.route('/canal', methods=['POST'])
-def post():
-    body =  request.json
-    return (Canal.post(body))
-
-@app.route('/canal', methods=['DELETE'])
-def delete():
-    body = request.json
-    return (Canal.delete(body))
->>>>>>> 4a410c7b4644a90b5a743415655b0d8f0820a488
 
 if __name__ == '__main__':
  app.run(port = 3000, debug=True)
